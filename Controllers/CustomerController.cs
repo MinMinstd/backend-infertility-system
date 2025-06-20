709174403490d431ee2225f876e640d71b8b7364
Cuti-Duck <khanhdnse180587@fpt.edu.vn>
2025-06-20 08:42:41 +0700

    Fix Update Profile

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -121,15 +121,26 @@ namespace infertility_system.Controllers
         public async Task<IActionResult> UpdateCustomerProfile(CustomerProfileDto customerProfileDto)
         {
             var UserIdClaims = Int32.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
-            if (!await _customerRepository.CheckExistsByUserId(UserIdClaims))
+            var exitCustomer = await _customerRepository.GetCustomersAsync(UserIdClaims);
+            if (exitCustomer == null)
                 return NotFound(new { Message = "Không tìm thấy dữ liệu khách hàng!" });
-           
+
+            customerProfileDto.FullName = customerProfileDto.FullName.Any() ? customerProfileDto.FullName : exitCustomer.FullName;
+            customerProfileDto.Email = customerProfileDto.Email.Any() ? customerProfileDto.Email : exitCustomer.Email;
+            customerProfileDto.Phone = customerProfileDto.Phone.Any() ? customerProfileDto.Phone : exitCustomer.Phone;
+            customerProfileDto.Gender = customerProfileDto.Gender.Any() ? customerProfileDto.Gender : exitCustomer.Gender;
+            customerProfileDto.Birthday = customerProfileDto.Birthday != null ? customerProfileDto.Birthday : exitCustomer.Birthday;
+            customerProfileDto.Address = customerProfileDto.Address.Any() ? customerProfileDto.Address : exitCustomer.Address;
+
             var user = _mapper.Map<User>(customerProfileDto);
             var updatedUser = await _userRepository.UpdateUser(UserIdClaims, user);
             if (updatedUser == null)
             {
                 return BadRequest(new { Message = "Cập nhật thông tin người dùng không thành công!" });
             }
+
+
+
 
             var customer = _mapper.Map<Customer>(customerProfileDto);
             var updatedCustomer = await _customerRepository.UpdateCutomerAsync(UserIdClaims, customer);
