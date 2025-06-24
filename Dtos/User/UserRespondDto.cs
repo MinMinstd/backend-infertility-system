5b3803d18cf9d4ef5027c3f5189d1031dd183b29
huy <huythse180580@fpt.edu.vn>
2025-06-24 17:46:33 +0700

    Update UserAPI
    - Update GetUserAfterLogin
    - Update data SQL

---
+++ Dtos/User/UserRespondDto.cs
@@ -0,0 +1,10 @@
+﻿namespace infertility_system.Dtos.User
+{
+    public class UserRespondDto
+    {
+        public int UserId { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Role { get; set; } // Assuming Role is a string, adjust as necessary
+    }
+}
