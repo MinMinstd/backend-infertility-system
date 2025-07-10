925561bb1f860bee84c0bd12be96d640f111c890
huy <huythse180580@fpt.edu.vn>
2025-07-10 23:35:32 +0700

    Update OrderDetailDto.cs
    Display StageName, DateTreatment, TimeTreatment for manager view

--- Dtos/OrderDetail/OrderDetailDto.cs
+++ Dtos/OrderDetail/OrderDetailDto.cs
@@ -3,7 +3,15 @@
     public class OrderDetailDto
     {
         public int OrderDetailId { get; set; }
+
         public string DoctorName { get; set; }
+
         public string ServiceName { get; set; }
+
+        public string StageName { get; set; }
+
+        public DateOnly DateTreatment { get; set; }
+
+        public string TimeTreatment { get; set; }
     }
 }
