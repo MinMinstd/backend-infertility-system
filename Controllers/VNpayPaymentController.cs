172072136c221451b731dd521b7f100b9c36fecc
Cuti-Duck <khanhdnse180587@fpt.edu.vn>
2025-07-19 02:31:26 +0700

    fix vnPay

--- Controllers/VNpayPaymentController.cs
+++ Controllers/VNpayPaymentController.cs
@@ -1,35 +1,42 @@
-﻿using infertility_system.Dtos.Vnpay;
-using infertility_system.Helpers;
-using infertility_system.Interfaces;
-using infertility_system.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿    using infertility_system.Dtos.Vnpay;
+    using infertility_system.Helpers;
+    using infertility_system.Interfaces;
+    using infertility_system.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
-namespace infertility_system.Controllers
-{
-    [Route("api/vnpay")]
-    [ApiController]
-    public class VNpayPaymentController : ControllerBase
+    namespace infertility_system.Controllers
     {
-        private readonly IVnpayService _vpnpayService;
-
-        public VNpayPaymentController(IVnpayService vpnpayService)
+        [Route("api/vnpay")]
+        [ApiController]
+        public class VNpayPaymentController : ControllerBase
         {
-            _vpnpayService = vpnpayService;
-        }
+            private readonly IVnpayService _vpnpayService;
 
-        [HttpPost("create")]
-        public IActionResult CreatePaymentUrlVnpay(VnpayPaymentInformationDto paymentInfor)
-        {
-           var url = _vpnpayService.CreatePaymentUrl(paymentInfor, HttpContext);
-            return Ok(url);
-        }
+            public VNpayPaymentController(IVnpayService vpnpayService)
+            {
+                _vpnpayService = vpnpayService;
+            }
 
-        [HttpGet("return")]
-        public IActionResult PaymentCallbackVnpay()
-        {
-            var response = _vpnpayService.PaymentExecute(Request.Query);
-            return Ok(response);
+            [HttpPost("create")]
+            public IActionResult CreatePaymentUrlVnpay(VnpayPaymentInformationDto paymentInfor)
+            {
+               var url = _vpnpayService.CreatePaymentUrl(paymentInfor, HttpContext);
+                return Ok(url);
+            }
+
+            [HttpGet("return")]
+            public IActionResult PaymentCallbackVnpay()
+            {
+                var response = _vpnpayService.PaymentExecute(Request.Query);
+
+                var redirectUrl = $"http://localhost:5173/payment-result" +
+                  $"?success={(response.Success ? "true" : "false")}" +
+                  $"&orderId={response.OrderId}" +
+                  $"&transactionId={response.TransactionId}" +
+                  $"&message={Uri.EscapeDataString(response.OrderDescription ?? "Không có mô tả")}";
+
+            return Redirect(redirectUrl);
+            }
         }
     }
-}
